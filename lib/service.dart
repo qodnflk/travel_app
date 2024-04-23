0f0509b14f879e1d79c1a4fb3f21f5e9588aff72
JinhoBae <qodnflk@naver.com>
2024-04-23 14:58:16 +0900

    first my travel app

---
+++ lib/service.dart
@@ -0,0 +1,28 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:study_app/user.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Services {
+  static String url = 'https://jsonplaceholder.typicode.com/users';
+
+  static Future<List<User>> getInfo() async {
+    
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final List<User> user = userFromJson(response.body);
+        return user;
+      } else {
+        Fluttertoast.showToast(msg: 'Error occurred. Please try again');
+        return <User>[];
+      }
+    } catch (e) {
+      Fluttertoast.showToast(msg: e.toString());
+      return <User>[];
+    }
+  }
+}
+
+
